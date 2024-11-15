8761ad2008a5c4fea0184c4f73bcdfaf198a4110
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2024-11-15 18:52:12 +0800

    Fix

--- Modules/Auth/app/Http/Controllers/AccountController.php
+++ Modules/Auth/app/Http/Controllers/AccountController.php
@@ -33,7 +33,7 @@ class AccountController extends Controller
                 AllowedFilter::callback('can_be_invited_to_project', function (Builder $builder, $value) use ($request) {
                     $builder->when($request->account, function (Builder $query) use ($value) {
                         $query->where(function (Builder $subQuery) use ($value) {
-                            $subQuery->whereDoesntHave('proposals', fn (Builder $proposalQuery) => $proposalQuery->where('project_id', $value))
+                            $subQuery->orWhereDoesntHave('proposals', fn (Builder $proposalQuery) => $proposalQuery->where('project_id', $value))
                                 ->orWhereDoesntHave('proposalInvitations', fn (Builder $proposalInvitationQuery) => $proposalInvitationQuery->where('project_id', $value));
                         });
                     });
