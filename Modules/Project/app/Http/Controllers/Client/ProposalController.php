19a1b8e00cdce6c22a794ad03fe3627c3928ff8d
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2024-11-20 21:58:52 +0800

    add

--- Modules/Project/app/Http/Controllers/Client/ProposalController.php
+++ Modules/Project/app/Http/Controllers/Client/ProposalController.php
@@ -83,7 +83,7 @@ class ProposalController extends Controller
      */
     public function show(Request $request, Proposal $proposal)
     {
-        $proposal->load(['project.account.user', 'attachments', 'contract']);
+        $proposal->load(['project' => ['account.user', 'languages', 'skills', 'images'], 'attachments', 'contract']);
 
         if ($request->account?->id != $proposal->project->account_id) {
             return response(['message' => 'Forbidden'], 403);
