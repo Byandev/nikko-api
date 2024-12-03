0c8b79b84d727c41e1218937d19c5f22e9a26c14
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2024-12-03 16:40:16 +0800

    FIX

--- Modules/Chat/app/Http/Controllers/ChannelController.php
+++ Modules/Chat/app/Http/Controllers/ChannelController.php
@@ -17,7 +17,7 @@ class ChannelController extends Controller
      */
     public function index(Request $request)
     {
-        $channels = Channel::with(['subject.project.account.user', 'members.avatar'])
+        $channels = Channel::with(['members.avatar'])
             ->whereHas('members', function (Builder $builder) {
                 $builder->where('users.id', Auth::id());
             })
