6835ac5e98e22b0c1756517a55736105a2064a2e
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2024-12-13 11:04:52 +0800

    Fix

--- Modules/Chat/app/Transformers/MessageResource.php
+++ Modules/Chat/app/Transformers/MessageResource.php
@@ -4,6 +4,7 @@ namespace Modules\Chat\Transformers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 use Modules\Auth\Transformers\UserResource;
 use Modules\Chat\Models\Message;
 use Modules\Media\Transformers\MediaResource;
@@ -25,6 +26,7 @@ class MessageResource extends JsonResource
             'content' => $this->content,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'sent_by_me' => Auth::check() && $this->sender_id == Auth::id(),
 
             'sender' => UserResource::make($this->whenLoaded('sender')),
             'attachments' => MediaResource::collection($this->whenLoaded('attachments')),
